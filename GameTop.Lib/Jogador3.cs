b0189a7c32cea900af7f21c0e1cfc4df39da9bf2
Petrônio Santos <petroniosoliveira@gmail.com>
2021-06-18 13:32:18 -0300

    first commit

---
+++ GameTop.Lib/Jogador3.cs
@@ -0,0 +1,22 @@
+using GameTop.Interface;
+
+namespace GameTop.Lib
+{
+    public class Jogador3 : iJogador
+    {
+        public string Chuta()
+        {
+            return " Teste Chuta \n";
+        }
+
+        public string Corre()
+        {
+            return " Teste Corre \n";
+        }
+
+        public string Passe()
+        {
+            return " Teste Passe \n";
+        }
+    }
+}
